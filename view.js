cfa250542b182ff0a7b51efd30fc620b8390e45b
csnoboa <caique2noboa@gmail.com>
2021-03-27 22:26:42 -0300

    minus button

---
+++ view.js
@@ -0,0 +1,11 @@
+let $ = require('jquery')  // jQuery now loaded and assigned to $
+let count = 0
+$('#click-counter').text(count.toString())
+$('#countbtn').on('click', () => {
+   count ++ 
+   $('#click-counter').text(count)
+}) 
+$('#countminusbtn').on('click', () =>{
+    count --
+    $('#click-counter').text(count)
+})
