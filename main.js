f82c4b2daab748016da7330e2bf6dddc5bfac6de
csnoboa <caique2noboa@gmail.com>
2021-03-27 22:15:28 -0300

    electron init

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron') 
+const url = require('url') 
+const path = require('path')  
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+let win  
 
-  win.loadFile('index.html')
-}
+function createWindow() { 
+   win = new BrowserWindow({width: 800, height: 600}) 
+   win.loadURL(url.format ({ 
+      pathname: path.join(__dirname, 'index.html'), 
+      protocol: 'file:', 
+      slashes: true 
+   })) 
+}  
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('ready', createWindow) 
