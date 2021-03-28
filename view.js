533aa91a45df03546026cf0f4d017b055ae68106
csnoboa <caique2noboa@gmail.com>
2021-03-27 23:29:45 -0300

    finish button

--- view.js
+++ view.js
@@ -17,3 +17,36 @@ $('#countbtnmore10').on('click', () => {
      count = count - 10
      $('#click-counter').text(count)
  })
+
+let count2 = 0
+$('#click-counter2').text(count2.toString())
+$('#countbtnmore2').on('click', () => {
+   count2 ++ 
+   $('#click-counter2').text(count2)
+}) 
+$('#countbtnminus2').on('click', () =>{
+    count2 --
+    $('#click-counter2').text(count2)
+})
+$('#countbtnmore210').on('click', () => {
+    count2 = count2 + 10 
+    $('#click-counter2').text(count2)
+ }) 
+ $('#countbtnminus210').on('click', () =>{
+     count2 = count2 - 10
+     $('#click-counter2').text(count2)
+ })
+
+ let textfinish = " "
+ $('#finishbtn').on('click', () => {
+     if (count > count2){
+        textfinish = "Player 1 Wins!!"
+     }
+     else if(count2 > count){
+         textfinish = "Player 2 Wins!!"
+     }
+     else{
+         textfinish = "Draw!"
+     }
+     $('#textfinish').text(textfinish)
+ })
