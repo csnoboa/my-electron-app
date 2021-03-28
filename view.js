d48b2b9e98720ed63457ae464b6454232c115eae
csnoboa <caique2noboa@gmail.com>
2021-03-27 22:39:29 -0300

    more butons!

--- view.js
+++ view.js
@@ -1,11 +1,19 @@
 let $ = require('jquery')  // jQuery now loaded and assigned to $
 let count = 0
 $('#click-counter').text(count.toString())
-$('#countbtn').on('click', () => {
+$('#countbtnmore').on('click', () => {
    count ++ 
    $('#click-counter').text(count)
 }) 
-$('#countminusbtn').on('click', () =>{
+$('#countbtnminus').on('click', () =>{
     count --
     $('#click-counter').text(count)
 })
+$('#countbtnmore10').on('click', () => {
+    count = count + 10 
+    $('#click-counter').text(count)
+ }) 
+ $('#countbtnminus10').on('click', () =>{
+     count = count - 10
+     $('#click-counter').text(count)
+ })
